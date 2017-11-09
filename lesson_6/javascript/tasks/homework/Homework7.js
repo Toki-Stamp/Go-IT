89b25fcde68aab4e4f52d12a5d99b5ad2b8ef523
Yuri Fomichev <fomichev@nca.by>
2017-11-10 01:36:41 +0300

    Super function

--- lesson_6/javascript/tasks/homework/Homework7.js
+++ lesson_6/javascript/tasks/homework/Homework7.js
@@ -8,38 +8,59 @@
 /* Массив, очищенный от анаграмм */
 
 (function main() {
-    var array = ['возов', 'киборг', 'корсет', 'ЗОВ', 'ГРОБИК', 'костер', 'СЕКТОР'];
+    var array = ['возов', 'киборг', 'корсет', 'ЗОВ', 'ГРОБИК', 'костер', 'СЕКТОР'],
+        result = [];
 
-    function anagramClean(array) {
-        var container = {},
-            hasNext = array.length,
-            index = 0,
-            item;
-        while (hasNext) {
-            item = array[index];
-            console.log(index + ':' + item + ':' + splitToChars(item));
-
-            index += 1;
-            hasNext -= 1;
+    array.forEach(function (item) {
+        result.push(uniqueCharsObject(item));
+    });
+    result.forEach(function (item, index) {
+        var key, value;
+        console.log('iteration: ' + index, ' object: ', item, ' length: ' + Object.keys(item).length);
+        for (key in item) {
+            if (item.hasOwnProperty(key)) {
+                value = item[key];
+                console.log(key + " = " + value);
+            }
         }
+    });
+    // [{a: 1, b: 2}, {c: 3, d: 4}].sort(byOccurrences);
+    //
+    // function byOccurrences(a, b) {
+    //     var aKey = Object.keys(a)[0],
+    //         aValue = a[aKey],
+    //         bKey = Object.keys(b)[0],
+    //         bValue = b[bKey];
+    //     // var result = 1;
+    //     console.log(aKey, ': ', aValue);
+    //     console.log(bKey, ': ', bValue);
+    //     // return result;
+    // }
 
-        console.log(countChars('aloha'));
-
-        function splitToChars(word) {
-            return word.split('');
-        }
-
-        function countChars(word, char) {
-            return splitToChars(word).reduce(function (occurrences, current) {
-                if (current === char) occurrences += 1;
-                return occurrences;
-            }, 0);
-
-        }
-
-        return array;
+    /**
+     * Разбивает слово word в массив букв
+     * @param {String} word - слово для разбиения
+     * @returns {Array} - массив букв
+     */
+    function chars(word) {
+        return word.split('');
     }
 
-    console.log(' Исходный массив:', array);
-    console.log('Очищенный массив:', anagramClean(array));
+    /**
+     * Создаёт объект уникальных букв {key: value} из входящего слова word, где
+     * key - уникальная буква, value - количество её повторений в исходном слове
+     * @param {String} word - входящее слово для поиска уникальных букв
+     * @returns {Object} - объект {key: value} уникальных букв на количесто её повторений
+     */
+    function uniqueCharsObject(word) {
+        var unique = {};
+        chars(word).forEach(function (char) {
+            char = char.toLowerCase();
+            unique[char] = (char in unique) ? ++unique[char] : 1;
+        });
+        return unique;
+    }
+
+    // console.log(' Входной массив:', array);
+    // console.log('Итоговый массив:', result);
 })();
